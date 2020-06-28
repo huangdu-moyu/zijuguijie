e1de21945250e22bcf16382f4c3f8789d67d6abf
ljl0222 <1021278241@qq.com>
2020-06-28 22:55:01 +0800

    稍微调了前端界面

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -16,8 +16,8 @@ MainWindow::MainWindow(QWidget *parent)
 
     ui->setupUi(this);
     //ui->InputNum->setStyleSheet("background:rgb(255,0,0,0);border:1px solid white;");
-    ui->show->setStyleSheet("background:rgb(255,0,0,0);border:1px solid white;");
-    ui->plainTextEdit->setStyleSheet("background:rgb(255,0,0,0);border:1px solid white;");
+    ui->show->setStyleSheet("background:rgb(255,0,0,0);border:1px solid white;border-radius: 10px;");
+    ui->plainTextEdit->setStyleSheet("background:rgb(255,0,0,0);border:1px solid white;border-radius: 10px;");
     for(int i=0;i<quesheng_size;i++)
     {
         ui->plainTextEdit->appendPlainText(quesheng[i]);
